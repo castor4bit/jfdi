2b64a0235399c68a7d732e66f22910fd2cc1973d
castor4bit <castor.4bit@gmail.com>
2015-03-28 14:30:36 +0900

    Hello World With Browserify

---
+++ 02_js_module_management/browserify/01_example.js
@@ -0,0 +1,9 @@
+var unique = require('uniq');
+
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+console.log(unique(data));
+
+// npm install uniq
+// npm install -g browserify
+// browserify 01_example.js -o 01_example_bundled.js
