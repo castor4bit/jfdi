6524cff91118f31cbe80b170185d3caa4ed149a1
castor4bit <castor.4bit@gmail.com>
2015-03-31 23:23:09 +0900

    Add Header/Footer Component

--- 03_martyjs_example/01_todomvc/app/components/TodoApp.react.js
+++ 03_martyjs_example/01_todomvc/app/components/TodoApp.react.js
@@ -1,9 +1,15 @@
 var React = require('react');
 
+var Footer = require('./Footer.react');
+var Header = require('./Header.react');
+
 var TodoApp = React.createClass({
   render: function() {
+    var allTodos = {};
     return (
       <div>
+        <Header />
+        <Footer allTodos={allTodos} />
       </div>
     );
   }
