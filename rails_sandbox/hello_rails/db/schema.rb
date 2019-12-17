d6dd31b3e3f727634ee78f0b18526c29a7ae1883
castor4bit <castor.4bit@gmail.com>
2019-12-17 11:21:46 +0900

    bundle exec rails g scaffold Link

--- rails_sandbox/hello_rails/db/schema.rb
+++ rails_sandbox/hello_rails/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_015914) do
+ActiveRecord::Schema.define(version: 2019_12_17_021653) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "url"
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
+    t.string "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
