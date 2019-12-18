0a738db92ef9aa87ae6434efaff5ae06867c4835
castor4bit <castor.4bit@gmail.com>
2019-12-18 11:34:06 +0900

    Add title (which fetched automatically)

--- rails_sandbox/hello_rails/app/models/link.rb
+++ rails_sandbox/hello_rails/app/models/link.rb
@@ -1,2 +1,14 @@
 class Link < ApplicationRecord
+  before_save :fetch_title
+
+  def fetch_title
+    begin
+      uri = URI.parse(url)
+      response = Net::HTTP::get(uri).force_encoding(Encoding::UTF_8)
+      captures = /<title>(?<title>.+)<\/title>/m.match(response).named_captures
+      self.title = captures['title']
+    rescue => e
+      logger.warn "Fetch Title Failed. url = [#{url}], error = #{e.message}"
+    end
+  end
 end
