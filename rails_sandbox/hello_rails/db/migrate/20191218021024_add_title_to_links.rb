bc9f0a587c45ca31fb3a865d655aaa9d5417fcd6
castor4bit <castor.4bit@gmail.com>
2019-12-18 11:13:38 +0900

    set position

--- rails_sandbox/hello_rails/db/migrate/20191218021024_add_title_to_links.rb
+++ rails_sandbox/hello_rails/db/migrate/20191218021024_add_title_to_links.rb
@@ -1,5 +1,5 @@
 class AddTitleToLinks < ActiveRecord::Migration[6.0]
   def change
-    add_column :links, :title, :string
+    add_column :links, :title, :string, limit: 512, after: :url
   end
 end
